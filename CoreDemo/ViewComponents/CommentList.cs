d805d8cbb17de4aec447fe9d30616de74832fe02
serhovski <sero41fb@outlook.com>
2023-03-07 01:39:50 +0300

    7 March Commit

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,31 @@
+﻿using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+	public class CommentList : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var commentvalues = new List<UserComment>
+			{
+				new UserComment
+				{
+					ID= 1,
+					UserName= "Serhat"
+				},
+				new UserComment
+				{
+					ID= 2,
+					UserName = "Uğurcan"
+				},
+				new UserComment
+				{
+					ID= 3,
+					UserName="Engin"
+				}
+			};
+			return View(commentvalues);
+		}
+	}
+}
