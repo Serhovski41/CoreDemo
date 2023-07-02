1e8fa0ef660d8d6d3444ec5b8225cecf2ba9ff54
serhovski <sero41fb@outlook.com>
2023-07-02 15:34:59 +0300

    2 July Commit

--- CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
+++ CoreDemo/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
@@ -29,9 +30,24 @@ namespace CoreDemo.Areas.Admin.Controllers
             return View(values);
         }
 
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            mm.TAdd(p);
+            return RedirectToAction("SendBox");
         }
     }
 }
