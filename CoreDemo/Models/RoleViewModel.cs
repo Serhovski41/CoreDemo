d6654cc781b6a04a0e8bc2a316f0b56fd2e0e46a
serhovski <sero41fb@outlook.com>
2023-07-04 01:21:50 +0300

    4 July Commit

---
+++ CoreDemo/Models/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreDemo.Models
+{
+    public class RoleViewModel
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen rol adı giriniz")]
+        public string name { get; set; }
+    }
+}
